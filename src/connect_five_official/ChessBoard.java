9361f595febf03fd7a3947a0ed718bc3fc9d776b
Lưu Thanh Sang <luuhero.sq@gmail.com>
2017-10-05 07:07:29 +0700

    update connect_five_official

--- src/connect_five_official/ChessBoard.java
+++ src/connect_five_official/ChessBoard.java
@@ -5,11 +5,14 @@
  */
 package connect_five_official;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -28,6 +31,9 @@ public class ChessBoard extends JPanel implements ActionListener {
 	public static JButton squares[][] = new JButton[row][column];
 	// Khởi tạo khung bàn cờ
 	public JFrame chessBoardFrame = new JFrame();
+	public JPanel board, menu;
+	public JButton human, computer;
+	public JLabel lblHuman, lblComputer;
 	
 	// Khai báo biến thế cờ
 	public String[][][] chessPositions = ChessPosition.getChessPostion("./src/connect_five_official/chess_positions.txt");
@@ -49,7 +55,11 @@ public class ChessBoard extends JPanel implements ActionListener {
 	 * Hàm vẽ bàn cờ
 	 */
 	public void drawChessBoard() {
-		chessBoardFrame.setLayout(new GridLayout(row, column)); // Thiết lập bố cục cho bàn cờ
+//		chessBoardFrame.setLayout(); // Thiết lập bố cục cho bàn cờ
+		board = new JPanel();
+		board.setLayout(new GridLayout(row, column));
+		board.setSize(900, 700);
+		board.setAlignmentX(LEFT_ALIGNMENT);
 		// Thêm các ô cờ vào bàn cờ
 		for (int i = 0; i < row; i++) { // duyệt qua từng hàng trong bàn cờ
 			for (int j = 0; j < column; j++) {
@@ -57,10 +67,28 @@ public class ChessBoard extends JPanel implements ActionListener {
 				squares[i][j] = new JButton(""); // Tạo, gán giá trị rỗng cho từng ô cờ
 				squares[i][j].setActionCommand(v); // Thiết lập chuỗi command gửi đi khi ô cờ được đánh, giúp xác định tọa độ ô cờ
 				squares[i][j].addActionListener(this); // Thêm sự kiện khi ô cờ được đánh
-				chessBoardFrame.add(squares[i][j]); // Thêm ô cờ vào bàn cờ
+				board.add(squares[i][j]); // Thêm ô cờ vào bàn cờ
 			}
 		}
-		chessBoardFrame.setSize(900, 900); // Thiết lập kích thước bàn cờ
+		
+		menu = new JPanel();
+		lblHuman = new JLabel("Người đánh");
+		human = new JButton("O");
+		human.setActionCommand("human");
+		human.addActionListener(this);
+		lblComputer = new JLabel("Máy đánh");
+		computer = new JButton("-");
+		computer.setActionCommand("computer");
+		computer.addActionListener(this);
+		menu.setSize(200, 700);
+		menu.setLayout(new FlowLayout());
+		menu.add(lblHuman);
+		menu.add(human);
+		menu.add(lblComputer);
+		menu.add(computer);
+		chessBoardFrame.add(board, BorderLayout.WEST);
+		chessBoardFrame.add(menu, BorderLayout.EAST);
+		chessBoardFrame.setSize(1200, 700); // Thiết lập kích thước bàn cờ
 		chessBoardFrame.setVisible(true); // Hiển thị bàn cờ
 	}
 	
@@ -71,22 +99,29 @@ public class ChessBoard extends JPanel implements ActionListener {
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		handle = new Controller(); // Khởi tạo đối tượng xử lí chơi cờ
 		int xPosition, yPosition; // Khởi tạo tung độ, hoành độ của ô cờ được đánh
 		String positionVal = e.getActionCommand(); // Lấy tọa độ ô cờ được đánh
-		String[] positionArr = positionVal.split(" "); // Lấy tung độ, hoành độ
-		handle = new Controller(); // Khởi tạo đối tượng xử lí chơi cờ
-		xPosition = Integer.parseInt(positionArr[0]); // Lấy hoành độ ô cờ được đánh
-		yPosition = Integer.parseInt(positionArr[1]); // Lấy tung độ ô cờ được đánh
-		// Kiểm tra ô cờ đánh có hợp lệ không
-		if ("".equals(squares[xPosition][yPosition].getText())) { // Nếu ô cờ được đánh vẫn trống
-			
-			squares[xPosition][yPosition].setText("O"); // Gán giá trị ô cờ được đánh là O
-			// Kiếm tra thắng thua
-			if (handle.checkWin(new Position(xPosition, yPosition))) { // Nếu người thắng
-				showMessage("You win!"); // Hiện thông báo thắng cuộc
-				handle.resetChessBoard(); // Tạo ván chơi mới
-			} else {
-				handle.getComputerTurn(chessPositions); // Nếu không thắng thì tới lượt máy đánh
+		if (e.getActionCommand() == "computer") {
+			handle.resetChessBoard(); // Tạo ván chơi mới
+			squares[9][9].setText("X");
+		} else if (positionVal == "human") {
+			handle.resetChessBoard(); // Tạo ván chơi mới
+		} else {
+			String[] positionArr = positionVal.split(" "); // Lấy tung độ, hoành độ
+			xPosition = Integer.parseInt(positionArr[0]); // Lấy hoành độ ô cờ được đánh
+			yPosition = Integer.parseInt(positionArr[1]); // Lấy tung độ ô cờ được đánh
+			// Kiểm tra ô cờ đánh có hợp lệ không
+			if ("".equals(squares[xPosition][yPosition].getText())) { // Nếu ô cờ được đánh vẫn trống
+				
+				squares[xPosition][yPosition].setText("O"); // Gán giá trị ô cờ được đánh là O
+				// Kiếm tra thắng thua
+				if (handle.checkWin(new Position(xPosition, yPosition))) { // Nếu người thắng
+					showMessage("You win!"); // Hiện thông báo thắng cuộc
+					handle.resetChessBoard(); // Tạo ván chơi mới
+				} else {
+					handle.getComputerTurn(chessPositions); // Nếu không thắng thì tới lượt máy đánh
+				}
 			}
 		}
 	}
